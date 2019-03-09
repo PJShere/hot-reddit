f13eda4460fdc86bd40aea82411c153abc8cad76
PJShere <pankajshere@gmail.com>
2019-03-09 15:52:29 +0530

    all good for now

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Subredditcontainer from './components/SubredditContainer';
+import './style.css';
+import Header from './components/Header';
+import Displaycont from './components/Displaycont'
+import { CSSTransitionGroup } from 'react-transition-group'
+//import Footer from './components/Footer';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+		constructor() {
+				super()
+				this.state ={
+					subreddit_list: [],
+					subreddit:"",
+					hot_list: [] 
+				}
+		this.getSubList = this.getSubList.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+		this.getSubHot = this.getSubHot.bind(this)
+		}
+
+
+		componentDidMount() {
+			this.getSubList()
+		}
+
+		getSubList() {
+		fetch("http://127.0.0.1:5000/hot-reddit/api/getsubreddits")
+		.then(response => response.json())
+		.then(data => { 
+			this.setState({
+					subreddit_list: data.sub_list
+			})				
+		})
+	}
+
+		getSubHot(value) {
+		var apiurl = 'http://127.0.0.1:5000/hot-reddit/api/gethotposts/'
+		var hoturl = apiurl.concat(value)
+		//console.log(hoturl)
+			fetch(hoturl)
+			.then(response => response.json())
+			.then(data => { 
+				this.setState({
+					hot_list: data.subreddit_hot_data
+			})				
+		})
+	}
+
+	updSubReddit(name, value) {
+			this.setState({
+				[name]: value
+			})
+	}
+
+	handleChange(event) {
+		const {name, value} = event.target	
+
+		this.updSubReddit(name, value);
+		this.getSubHot(value);
+	}
+
+		render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+				<Header />
+				<p>Choose a subreddit and see whats hot</p>
+				<br />
+			 <Subredditcontainer 
+					handleChange={this.handleChange}
+					subreddit={this.state.subreddit}
+					subreddit_list={this.state.subreddit_list}
+			/>
+			<br />
+			<h4>{this.state.subreddit}</h4>
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+					{this.state.hot_list.length > 0 ? 
+					<Displaycont hot_list={this.state.hot_list} /> : null}
+        </CSSTransitionGroup>
+	</div>
+
+      )
+	
+	}
 }
 
 export default App;
