c90791896c86a84cd61b13bef42d5abe07fc74d2
PJShere <pankajshere@gmail.com>
2019-03-14 11:14:31 +0530

    working app

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends React.Component {
 							/>
 							<br />
 							{isloading ? <div id="fountainG"> 
-								<div id="fountainG_1" className="fountainG"></div>
+									<div id="fountainG_1" className="fountainG"></div>
 									<div id="fountainG_2" className="fountainG"></div>
 									<div id="fountainG_3" className="fountainG"></div>
 									<div id="fountainG_4" className="fountainG"></div>
